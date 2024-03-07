1a0be0a39f463455090e42626b67313f09da8501
AaTherf <it.atharv.m39@gmail.com>
2024-03-07 20:22:09 +0530

    Starting the bot

---
+++ Aves.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds]})
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Logged in as ${readyClient.user.tag}`);
+})
+
+client.login(process.env.TOKEN)
